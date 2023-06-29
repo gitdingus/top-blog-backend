8d64fa85bebd884c3c357d6b51f0e122bddbbe67
Josh S <aaabccc87@protonmail.com>
2023-06-29 10:49:47 -0400

    Comment routes

--- routes/accounts.js
+++ routes/accounts.js
@@ -3,10 +3,12 @@ const accountsController = require('../controllers/accountsController.js');
 
 const accountsRouter = express.Router();
 
+// Account creation and authentication
 accountsRouter.post('/api/create-account', accountsController.api_post_create_account);
 accountsRouter.post('/api/login', accountsController.api_post_login);
 accountsRouter.post('/api/logout', accountsController.api_post_logout);
 
+// User profile
 accountsRouter.get('/api/users/:username', accountsController.api_get_user_profile);
 
 // Update account information
