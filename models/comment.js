adedaaa8da122de63f65a1436c9fd57e47a07f34
Josh S. <aaabccc87@protonmail.com>
2023-08-28 13:51:41 -0400

    Export model for comments

---
+++ models/comment.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const commentSchema = new Schema({
+  blogPost: {
+    required: true,
+    type: Schema.Types.ObjectId,
+    ref: 'BlogPost',
+  },
+  author: {
+    required: true,
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+  },
+  created: {
+    type: Date,
+    required: true,
+  },
+  title: {
+    type: String,
+    trim: true,
+    maxLength: 50,
+    required: true,
+  },
+  content: {
+    type: String,
+    trim: true,
+    maxLength: 1000,
+    required: true,
+  }
+});
+
+module.exports = mongoose.model('Comment', commentSchema);
