60e368cb53add907ad42b047132533acb363993b
Josh S. <aaabccc87@protonmail.com>
2023-09-12 19:05:18 -0400

    api_get_reports: return reports based on query parameters

--- controllers/reportsController.js
+++ controllers/reportsController.js
@@ -1,12 +1,26 @@
 const asyncHandler = require('express-async-handler');
 const createError = require('http-errors');
-const { body, param, validationResult } = require('express-validator');
+const { body, query, validationResult } = require('express-validator');
 const express = require('express');
 const Report = require('../models/report.js');
 
 const isLoggedIn = (req, res, next) => {
   if (!req.isAuthenticated()) {
-    return next(createError(401, "Unauthorized"));
+    return next(createError(401, 'Unauthorized'));
+  }
+
+  next();
+}
+
+const isAdminOrModerator = (req, res, next) => {
+  const allowedTypes = ['Admin', 'Moderator'];
+
+  if (!req.isAuthenticated()) {
+    return next(createError(401, 'Unauthorized'));
+  }
+
+  if (!allowedTypes.includes(req.user.accountType)) {
+    return next(createError(403, 'Forbidden'));
   }
 
   next();
@@ -49,7 +63,49 @@ exports.api_post_reports = [
 ];
 
 exports.api_get_reports = [
+  isAdminOrModerator,
+  query('settled', 'Settled must be boolean value')
+    .optional()
+    .isBoolean({ strict: true }),
+  query('contentType', 'Content type must be "comment" or "blogpost"')
+    .optional()
+    .custom((val) => {
+      const contentTypes = ['comment', 'blogpost'];
+
+      return contentTypes.includes(val.toLower());
+    }),
+  query('contentId')
+    .optional()
+    .isMongoId()
+    .withMessage('contentId must be valid mongoId')
+    .custom((val, { req }) => {
+      return req.query.contentType !== undefined;
+    })
+    .withMessage('Must specify contentType if using contentId'),
   asyncHandler(async (req, res, next) => {
-    res.status(200).json({ msg: "api_get_reports: Not implemented"});
+    const errors = validationResult(req);
+
+    if(!errors.isEmpty()) {
+      res.status(400).json({ errors: errors.array() });
+      return;
+    }
+
+    const matchObj = {};
+
+    if (req.query.settled !== undefined) {
+      matchObj.settled = req.query.settled;
+    }
+
+    if (req.query.contentType !== undefined) {
+      matchObj.contentType = req.query.contentType;
+    }
+
+    if (req.query.contentId !== undefined) {
+      matchObj.contentId = req.query.contentId;
+    }
+
+    const reports = await Report.find( matchObj ).exec();
+
+    res.status(200).json(reports);
   }),
 ];
